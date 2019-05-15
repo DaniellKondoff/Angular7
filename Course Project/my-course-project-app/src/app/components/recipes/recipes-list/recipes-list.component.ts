e5597863eba92db79118a038c721f4b8ec763bb5
DKondoff <danielkondov91gmail.com>
2019-05-15 15:46:05 +0300

    Course Project

---
+++ Course Project/my-course-project-app/src/app/components/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe } from '../models/recipe.model'
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Array<Recipe> = [
+    new Recipe('A Test Recipe', 'This is a simply a test', 'https://food-images.files.bbci.co.uk/food/recipes/really_good_kedgeree_75198_16x9.jpg'),
+    new Recipe('A Test Recipe 2', 'This is a simply a test 2', 'https://food-images.files.bbci.co.uk/food/recipes/really_good_kedgeree_75198_16x9.jpg')
+  ];
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
